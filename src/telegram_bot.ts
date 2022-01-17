7cc03c7aa07159f6c3ed7708706bdd658edc2e40
ThePr0phet <niklaskoch4@googlemail.com>
2022-01-17 16:13:52 +0100

    Telegram-Bot working Basic

--- src/telegram_bot.ts
+++ src/telegram_bot.ts
@@ -1,44 +1,19 @@
-import { Bot } from "https://deno.land/x/telegram@v0.1.1/mod.ts";
-import {TelegramBot, UpdateType } from "https://deno.land/x/telegram_bot_api/mod.ts";
-import "https://deno.land/x/dotenv/load.ts";
+// import { TelegramBot, UpdateType } from "https://deno.land/x/telegram_bot_api/mod.ts"
+import { TelegramBot, UpdateType } from "https://raw.githubusercontent.com/michael-spengler/deno-telegram-bot-api/master/mod.ts"
+import "https://deno.land/x/dot_env@0.2.0/load.ts"
 
-const token = Deno.env.get("TOKEN");
+const TOKEN = Deno.env.get("TOKEN");
+if (!TOKEN) throw new Error("Bot token is not provided");
+const bot = new TelegramBot(TOKEN);
 
-const bot = new TelegramBot("5084168171:AAFynbkC-112hpy8hmssBcJONjDsCuMZvHI");
+bot.on(UpdateType.Message, async (message: any) => {
 
-bot.on(UpdateType.Message, async ({ message }) => {
-    const chatID = message.chat.id;
-    console.log("Chat-ID: " + chatID)
-    await bot.sendMessage({
-        chat_id: chatID,
-        text: "Dog or Cat?",
-        reply_markup: {
-          inline_keyboard: [
-            [
-              { text: "Dog", callback_data: "dog" },
-              { text: "Cat", callback_data: "cat" },
-            ],
-          ],
-        },
-      });
-    });
+    const text = message.message.text || "I can't hear you";
+    console.log(message)
+    await bot.sendMessage({ chat_id: -713194120, text: `echo ${text}` })
 
+});
 
-    bot.on(UpdateType.CallbackQuery, async ({ callback_query }) => {
-        const { id, data, from } = callback_query;
-        const text = data === "dog" ? "Wrong Answer" : "Correct Answer.";
-      
-        await bot.sendMessage({
-          chat_id: from.id,
-          text,
-        });
-      
-        await bot.answerCallbackQuery({
-          callback_query_id: id,
-        });
-      });
-      
-      bot.on(
-        UpdateType.Error,
-        ({ error }) => console.error("Error Occured", error.stack),
-      );
+bot.run({
+    polling: true,
+});
