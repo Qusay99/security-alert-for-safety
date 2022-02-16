6c6183ff956ec510fee6d8f6c1acc25269363f86
qusay99 <qusay.hamid9@gmail.com>
2022-02-16 23:30:37 +0100

    deleted unuseful code lines

--- src/distance_calculator.ts
+++ src/distance_calculator.ts
@@ -8,13 +8,10 @@ export class NearestCityCalculator {
 
     // retrieve the location_to_chat_IDs.json
     public static async getCityJSON() {
-    // public async getCityJSON() {
         let x = await Request.get('https://raw.githubusercontent.com/Qusay99/security-alert-for-safety/main/location_to_chat_IDs.json');
         return x
     }
     public static async findNearestCity(userGeo: any){
-    // public static async findNearestCity(userGeo: number[]) {
-    // public async findNearestCity(userGeo: any) {
         var citiesgeo: any;
         var distance: number;
         var location: any;
@@ -31,20 +28,13 @@ export class NearestCityCalculator {
                 x = Object.values(entry)[1]
                 let long = Object.values(x[0])[0];
                 let lat = Object.values(x[0])[1];
-                // let calculated_distance = DistanceCalculator.getDistanceInKilometers(Number(lat), Number(long), userGeo.latitude, userGeo.longitude)
                 let calculated_distance = DistanceCalculator.getDistanceInKilometers(Number(lat), Number(long), userGeo[0], userGeo[1])
                 if (calculated_distance < distance){
                     distance = calculated_distance
                     location = Object.values(entry)[0]
-                    // console.log(location)
                 }
             }
         }
         return location
     }
 }
-// import { NearestCityCalculator } from './distance_calculator.ts';
-
-// const city =  await NearestCityCalculator.findNearestCity({latitude: 48.137154, longitude: 11.46694})
-// const city =  await NearestCityCalculator.findNearestCity([49.4719058, 8.4852408])
-// console.log(city)
